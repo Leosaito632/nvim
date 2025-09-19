ae7a36cbb65aa3743d0fe9d54bfb961193a4a367
Leonardo Saito <lsaito@mppr.mp.br>
2025-09-19 14:23:41 -0300

    fix deprecated lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,22 +18,15 @@ return {
 
 config = function()
   local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
   -- Garante que os servidores listados estejam instalados.
-  -- Vi que você tem awk_ls e pyright instalados, então adicionei ambos.
   require("mason-lspconfig").setup({
-    ensure_installed = { "pyright", "awk_ls" },
+    ensure_installed = { "pyright"},
   })
 
   -- Configuração explícita para cada servidor que você quer usar.
-  vim.lsp.config.pyright.setup({
+  vim.lsp.config('pyright',{
     capabilities = capabilities,
   })
-
-  vim.lsp.config.awk_ls.setup({
-    capabilities = capabilities,
-  })
-
   -- Atalhos de Teclado Essenciais para LSP
   vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
